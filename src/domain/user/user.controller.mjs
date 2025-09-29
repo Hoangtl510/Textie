efa6c7d755167ea316a4c002f331d1cec1081d21
hoangtl <hoangtl@famtech.net>
2025-09-29 17:04:13 +0700

    add multer upload file

--- src/domain/user/user.controller.mjs
+++ src/domain/user/user.controller.mjs
@@ -16,14 +16,16 @@ export const profile = async (req, res) => {
 };
 
 export const updateProfile = async (req, res) => {
-  try {
-    await updateProfileService(req.body, req.user_id);
-    res.status(200).json({ success: true });
-  } catch (err) {
-    res
-      .status(err.status ?? 500)
-      .json({ error: err.status ?? 500, message: err.message });
-  }
+  //   try {
+  //     await updateProfileService(req.body, req.user_id);
+  //     res.status(200).json({ success: true });
+  //   } catch (err) {
+  //     res
+  //       .status(err.status ?? 500)
+  //       .json({ error: err.status ?? 500, message: err.message });
+  //   }
+  console.log("req", req.file);
+  console.log("req", req.body);
 };
 
 export const getUserById = async (req, res) => {};
--- src/domain/user/user.routes.mjs
+++ src/domain/user/user.routes.mjs
@@ -1,11 +1,13 @@
 import express from "express";
 import { profile, updateProfile, getUserById } from "./user.controller.mjs";
 import { auth } from "../../middleware/auth.mjs";
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 router.use(auth);
 router.get("/profile", profile);
-router.put("/update-profile", updateProfile);
+router.put("/update-profile", upload.single("avatar"), updateProfile);
 router.get("/user", getUserById);
 
 export default router;
